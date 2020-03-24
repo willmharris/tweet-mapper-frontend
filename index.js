56b2ce3da25c5d13ebd12427b5f3484c7f234c5c
willmharris <willmharris0@gmail.com>
2020-03-24 11:22:42 -0400

    got basic current search, past search, and save search functionality working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   addSearchFunctions()
+  addModal()
 })
 
 function addSearchFunctions() {
@@ -27,6 +28,7 @@ function renderSearchResults(data) {
   tweetDates1.forEach(date => getDateCount(date, countResult1))
   tweetDates2.forEach(date => getDateCount(date, countResult2))
   renderChart(countResult1, countResult2)
+  renderSearch(data)
 }
 
 function getDateCount(date, countResult) {
@@ -60,7 +62,7 @@ function renderChart(countResult1, countResult2) {
     },
     // Configuration options go here
     options: {}
-});
+  })  
 }
 
 function putDatesInArray(countResult) {
@@ -78,3 +80,81 @@ function putUsesInArray(countResult) {
   }
   return usesArray
 }
+
+function addModal() {
+  const modal = document.getElementById("myModal");
+  modal.style.display = "block"
+  document.querySelector('#username').addEventListener('submit', (event) => {
+    event.preventDefault()
+    modal.style.display = "none" 
+    fetch(`http://localhost:3000/users/?name=${event.target.children[1].value}`).then(
+      resp => resp.json()
+    ).then(data => renderFavorites(data))
+  })
+}
+
+function renderFavorites(userData) {
+  document.querySelector('h1').id = userData.id 
+  favoritesArea = document.querySelector('#past-search-area')
+  let searches = userData.searches
+  searches.forEach(search => {
+    listItem = document.createElement('li')
+    listItem.innerText = search.hashtags
+    listItem.dataset.searchId = search.id
+    listItem.addEventListener("click", displaySearch)
+
+    deleteBtn = document.createElement('button')
+    deleteBtn.addEventListener('click', deleteSearch)
+    
+    listItem.append(deleteBtn)
+    favoritesArea.append(listItem)
+  })
+}
+
+function displaySearch(event) {
+  form = document.querySelector('#chart_form')
+  searches = event.target.innerText.split(' ')
+  form.children[5].value = searches[0]
+  form.children[7].value = searches[1]
+  form.children[8].click()
+}
+
+function deleteSearch(event) {
+  searchId = event.target.parentElement.dataset.searchId 
+  fetch(`http://localhost:3000/searches/${searchId}`, {
+    method: "DELETE"
+  }).then(event.target.parentElement.remove())
+}
+
+function renderSearch(data) {
+  hashtag1 = data[0][0].hashtag
+  hashtag2 = data[1][0].hashtag
+  search = `${hashtag1} ${hashtag2}`
+  
+  listItem = document.createElement('li')
+  listItem.innerText = search 
+
+  saveBtn = document.createElement('button')
+  saveBtn.addEventListener("click", saveSearch)
+
+  listItem.append(saveBtn)
+  searchArea = document.querySelector("#current-search-area")
+  searchArea.append(listItem)
+}
+
+function saveSearch(event) {
+  hashtags = event.target.parentElement.innerText
+  userId = document.querySelector('h1').id
+  fetch("http://localhost:3000/searches", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json", 
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({ 
+      user_id: userId, 
+      hashtags: hashtags
+    })
+  })
+}
+
