a7f521723be38650f890b1f9b3a9d2e85b60fe65
willmharris <willmharris0@gmail.com>
2020-03-25 03:05:37 -0400

    got itertive hashtag functionality from the input to the rails controller, but not with rendering the chart yet

--- index.js
+++ index.js
@@ -1,26 +1,66 @@
 document.addEventListener("DOMContentLoaded", () => {
-  addSearchFunctions()
-  addModal()
+  // Allows the search form to accept input, transmit data to the server, and render a chart in response
+  document.querySelector("#search-form").addEventListener("submit", addSearchFunction)
+  // Allows the user to add additional hashtag inputs 
+  document.querySelector("#add-hashtag").addEventListener("click", addHashtags)
+  // Adds login screen 
+  addLogin()
 })
 
-function addSearchFunctions() {
-  let form = document.querySelector("form")
-  form.addEventListener("submit", (event) => {
-    event.preventDefault()
-    let startDate = event.target.children[1].value
-    let endDate = event.target.children[3].value
-    let hashtag1 = event.target.children[5].value
-    let hashtag2 = event.target.children[7].value
-    fetch(`http://localhost:3000/tweets/?hashtag1=${hashtag1}&hashtag2=${hashtag2}&start=${startDate}&end=${endDate}`).then(
-      resp => resp.json()
-    ).then(
-      data => renderSearchResults(data)
-    )
+function addSearchFunction(event) {
+  event.preventDefault()
+  // Get start and end dates from the form values 
+  let startDate = document.querySelector("#start-date").value
+  let endDate = document.querySelector("#end-date").value
+  // Get hashtags from the form values 
+  let hashtagInputArray = document.querySelectorAll(".hashtag-input")
+  let hashtagValueArray = Array.from(hashtagInputArray).map(hashtagInput => hashtagInput.value)
+  // Put hashtags into a JSON format 
+  let payload = {start: startDate, end: endDate}
+  let counter = 1 
+  hashtagValueArray.forEach(hashtag => {
+    payload[`hashtag${counter}`] = hashtag 
+    counter ++ 
   })
+  // Requests the server for the search parameters and render a chart based on the returned information 
+  fetch("http://localhost:3000/tweets", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json", 
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      payload
+    })
+  }).then(
+    resp => resp.json()
+  ).then(
+    data => renderSearchResults(data)
+  )
+}
+
+function addHashtags(event) {
+  event.preventDefault() 
+  // Get the search form 
+  searchForm = document.querySelector("#search-form")
+  // Make the new label
+  newHashtagLabel = document.createElement('label')
+  newHashtagLabel.innerText = `Hashtag ${currentHashtagNumber}:`
+  // Make the new input area
+  newHashtagInput = document.createElement("input")
+  newHashtagInput.type = "text"
+  newHashtagInput.classList.add("hashtag-input")
+  // Find the current location in the form I should append the input to
+  searchFormLength = searchForm.children.length 
+  currentNumberOfHashtags = (searchFormLength - 6)/2
+  currentHashtagNumber = currentNumberOfHashtags + 1 
+  // Append the input 
+  searchForm.insertBefore(newHashtagInput, searchForm.children[searchFormLength - 2])
+  searchForm.insertBefore(newHashtagLabel, searchForm.children[searchFormLength - 2])
 }
 
 function renderSearchResults(data) {
-  let chartArea = document.querySelector("#chart-area")
+  debugger 
   let tweetDates1 = data[0].map(tweet => tweet.date)
   let tweetDates2 = data[1].map(tweet => tweet.date)
   let countResult1 = {}
@@ -81,7 +121,7 @@ function putUsesInArray(countResult) {
   return usesArray
 }
 
-function addModal() {
+function addLogin() {
   const modal = document.getElementById("myModal");
   modal.style.display = "block"
   document.querySelector('#username').addEventListener('submit', (event) => {
@@ -95,7 +135,7 @@ function addModal() {
 
 function renderFavorites(userData) {
   document.querySelector('h1').id = userData.id 
-  favoritesArea = document.querySelector('#past-search-area')
+  favoritesArea = document.querySelector('#favorite-searches')
   let searches = userData.searches
   searches.forEach(search => {
     listItem = document.createElement('li')
@@ -112,7 +152,7 @@ function renderFavorites(userData) {
 }
 
 function displaySearch(event) {
-  form = document.querySelector('#chart_form')
+  form = document.querySelector('#search-form')
   searches = event.target.innerText.split(' ')
   form.children[5].value = searches[0]
   form.children[7].value = searches[1]
@@ -138,7 +178,7 @@ function renderSearch(data) {
   saveBtn.addEventListener("click", saveSearch)
 
   listItem.append(saveBtn)
-  searchArea = document.querySelector("#current-search-area")
+  searchArea = document.querySelector("#recent-searches")
   searchArea.append(listItem)
 }
 
